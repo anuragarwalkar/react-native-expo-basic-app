090a2a6f532aceeb8b9b43096455f1b9882edd90
Anurag <anuragarwalkar@gmail.com>
2021-03-08 01:16:12 +0530

    adde dpermissions

--- src/components/UI/ImageSelector.tsx
+++ src/components/UI/ImageSelector.tsx
@@ -1,16 +1,27 @@
 import { launchCameraAsync } from 'expo-image-picker';
+import { askAsync, CAMERA, MEDIA_LIBRARY } from 'expo-permissions';
 import React, { FC, useState } from 'react';
-import { Button, Image, StyleSheet, Text, View, ViewStyle } from 'react-native';
+import { Alert, Button, Image, StyleSheet, Text, View, ViewStyle } from 'react-native';
 import appColors from '../../constants/appColors';
 
 const ImageSelector: FC<{ styles?: ViewStyle; onImageSelect: (path: string) => void }> = (props) => {
   const [takenImage, setTakenImage] = useState('');
 
+  const verifyPermissions = async () => {
+    const result = await askAsync(CAMERA, MEDIA_LIBRARY);
+    if (result.status !== 'granted') {
+      Alert.alert('Insufficient Permissions', 'You need to grant camera permissions', [{ text: 'Okay' }]);
+      return false;
+    }
+    return true;
+  };
   const takeImageHandler = async () => {
-    const image: any = await launchCameraAsync();
-    if (!image.cancelled) {
-      setTakenImage(image.uri);
-      props.onImageSelect(image.uri);
+    if (verifyPermissions()) {
+      const image: any = await launchCameraAsync();
+      if (!image.cancelled) {
+        setTakenImage(image.uri);
+        props.onImageSelect(image.uri);
+      }
     }
   };
   return (
