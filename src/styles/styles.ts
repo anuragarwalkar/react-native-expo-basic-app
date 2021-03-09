21125d6e58fa87772936239a9254400e283bef00
Anurag <anuragarwalkar@gmail.com>
2021-03-09 11:15:31 +0530

    custom styles

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -1,3 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 export const textStyles = StyleSheet.create({});
+
+export const globalStyles = StyleSheet.create({
+  absoluteCenter: { flex: 1, justifyContent: 'center', alignItems: 'center' },
+});
