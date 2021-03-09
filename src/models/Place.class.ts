48f44e40803dfe9ef64f250f85bc1f1f2c3a93f7
Anurag <anuragarwalkar@gmail.com>
2021-03-09 18:27:19 +0530

    added details screen

--- src/models/Place.class.ts
+++ src/models/Place.class.ts
@@ -1,3 +1,3 @@
 export default class Place {
-  constructor(public id: number, public title: string, public image: string) {}
+  constructor(public id: number, public title: string, public image: string, public address: string) {}
 }
--- src/screens/PlaceDetails.tsx
+++ src/screens/PlaceDetails.tsx
@@ -1,22 +1,35 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 import { NavigationStackScreenComponent } from 'react-navigation-stack';
-
-interface PlaceDetailsParams {
-  id: number;
-  title: string;
-}
+import { useSelector } from 'react-redux';
+import RootState from '../models/redux/RootState.model';
 
 const PlaceDetails: NavigationStackScreenComponent = (props) => {
   const itemId = props.navigation.getParam('id');
+
+  const place = useSelector((state: RootState) => state.places.places.find((item) => item.id === itemId));
+
   return (
-    <View>
-      <Text></Text>
+    <View style={styles.container}>
+      <Image source={{ uri: place?.image }} style={styles.image} />
+      <View style={{ alignItems: 'center', marginTop: 20 }}>
+        <Text style={{ fontSize: 20 }}>{place?.title}</Text>
+      </View>
+      <View style={{ alignItems: 'center', marginTop: 10 }}>
+        <Text>Address: {place?.address}</Text>
+      </View>
     </View>
   );
 };
 
 export default PlaceDetails;
+
+const styles = StyleSheet.create({
+  image: { width: '100%', height: 200 },
+  container: {
+    padding: 20,
+  },
+});
 
 PlaceDetails.navigationOptions = (navData) => {
   const headerTitle = navData.navigation.getParam('title');
@@ -24,5 +37,3 @@ PlaceDetails.navigationOptions = (navData) => {
     headerTitle,
   };
 };
-
-const styles = StyleSheet.create({});
