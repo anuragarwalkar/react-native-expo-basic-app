4b28dad0be709281be628abc40d1ac1e5b20c074
Anurag <anuragarwalkar@gmail.com>
2021-03-12 17:27:07 +0530

    added push notification

--- App.tsx
+++ App.tsx
@@ -1,25 +1,80 @@
-import { scheduleNotificationAsync } from 'expo-notifications';
+import {
+  addNotificationReceivedListener,
+  addNotificationResponseReceivedListener,
+  getExpoPushTokenAsync,
+  setNotificationHandler,
+} from 'expo-notifications';
 import { NOTIFICATIONS } from 'expo-permissions';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, StyleSheet, Text, View } from 'react-native';
 import { getPermissions } from './src/helpers/utilityFunctions';
 
+setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+      shouldPlaySound: true,
+      shouldSetBadge: true,
+    };
+  },
+});
+
 const App = () => {
+  const [token, setToken] = useState('');
   useEffect(() => {
-    getPermissions(NOTIFICATIONS).then((res) => {
-      console.log('res:', res);
-    });
+    getPermissions(NOTIFICATIONS)
+      .then((res) => {
+        return getExpoPushTokenAsync();
+      })
+      .then((res) => {
+        setToken(res.data);
+        console.log('Expo Token:', res.data);
+      });
   }, []);
-  const triggerNotification = async () => {
-    await scheduleNotificationAsync({
-      content: {
-        title: 'my first notification',
-        body: 'this is my first notificaiton',
-      },
-      trigger: {
-        seconds: 10,
-      },
+
+  useEffect(() => {
+    const subscription = addNotificationReceivedListener((notification) => {
+      console.log('Forground Notification:', notification);
     });
+
+    const backgroundSubscription = addNotificationResponseReceivedListener((res) => {
+      console.log('Background Notification:', res);
+    });
+
+    return () => {
+      subscription.remove();
+      backgroundSubscription.remove();
+    };
+  }, []);
+
+  const triggerNotification = async () => {
+    // await scheduleNotificationAsync({
+    //   content: {
+    //     title: 'my first notification',
+    //     body: 'this is my first notificaiton',
+    //   },
+    //   trigger: {
+    //     seconds: 10,
+    //   },
+    // });
+
+    try {
+      await fetch('https://exp.host/--/api/v2/push/send', {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Accept-Encoding': 'gzip, deflate',
+          'content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          to: token,
+          title: 'Sent via the app',
+          body: 'this push was sent via app',
+        }),
+      });
+    } catch (error) {
+      console.log('error:', error);
+    }
   };
   return (
     <View style={styles.container}>
